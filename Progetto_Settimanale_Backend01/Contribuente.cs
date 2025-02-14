c56441eedc8b3d57075d9bd63ea6676c3b4a0d46
Jonathan130900 <jlb130900@gmail.com>
2025-02-14 11:34:26 +0100

    Progetto completato

--- Progetto_Settimanale_Backend01/Contribuente.cs
+++ Progetto_Settimanale_Backend01/Contribuente.cs
@@ -1,30 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Progetto_Settimanale_Backend01
+public class Contribuente
 {
-    public class Contribuente
-    {
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public DateTime DataNascita { get; set; }
-        public string CodiceFiscale { get; set; }
-        public char Sesso { get; set; }
-        public string ComuneResidenza { get; set; }
-        public decimal RedditoAnnuale { get; set; }
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+    public DateTime DataNascita { get; set; }
+    public string CodiceFiscale { get; set; }
+    public char Sesso { get; set; }
+    public string ComuneResidenza { get; set; }
+    public decimal RedditoAnnuale { get; set; }
 
-        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, decimal redditoAnnuale)
-        {
-            Nome = nome;
-            Cognome = cognome;
-            DataNascita = dataNascita;
-            CodiceFiscale = codiceFiscale;
-            Sesso = sesso;
-            ComuneResidenza = comuneResidenza;
-            RedditoAnnuale = redditoAnnuale;
-        }
+    public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, decimal redditoAnnuale)
+    {
+        Nome = nome;
+        Cognome = cognome;
+        DataNascita = dataNascita;
+        CodiceFiscale = codiceFiscale;
+        Sesso = sesso;
+        ComuneResidenza = comuneResidenza;
+        RedditoAnnuale = redditoAnnuale;
+    }
+
+
+    public decimal CalcolaImposta()
+    {
+        decimal imposta = 0;
+
+        if (RedditoAnnuale <= 15000)
+            imposta = RedditoAnnuale * 0.23m;
+        else if (RedditoAnnuale <= 28000)
+            imposta = (15000 * 0.23m) + ((RedditoAnnuale - 15000) * 0.27m);
+        else if (RedditoAnnuale <= 55000)
+            imposta = (15000 * 0.23m) + (13000 * 0.27m) + ((RedditoAnnuale - 28000) * 0.38m);
+        else if (RedditoAnnuale <= 75000)
+            imposta = (15000 * 0.23m) + (13000 * 0.27m) + (27000 * 0.38m) + ((RedditoAnnuale - 55000) * 0.41m);
+        else
+            imposta = (15000 * 0.23m) + (13000 * 0.27m) + (27000 * 0.38m) + (20000 * 0.41m) + ((RedditoAnnuale - 75000) * 0.43m);
+
+        return imposta;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome} {Cognome}\n" +
+               $"Data di nascita: {DataNascita:dd-MM-yyyy} ({Sesso})\n" +
+               $"Codice Fiscale: {CodiceFiscale}\n" +
+               $"Comune di residenza: {ComuneResidenza}\n" +
+               $"Reddito annuale dichiarato: {RedditoAnnuale:N2} €\n" +
+               $"IMPOSTA DA VERSARE: {CalcolaImposta():N2} €";
     }
 }
--- Progetto_Settimanale_Backend01/Program.cs
+++ Progetto_Settimanale_Backend01/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" BENVENUTO NEL CALCOLATORE DELL'IMPOSTA DA VERSARE");
+            Console.WriteLine("==================================================\n");
+
+
+            Console.Write("Inserisci il nome del Contribuente: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Inserisci il cognome del Contribuente: ");
+            string cognome = Console.ReadLine();
+
+            DateTime dataDiNascita;
+            while (true)
+            {
+                Console.Write("Inserisci la data di nascita (gg-mm-yyyy): ");
+                if (DateTime.TryParse(Console.ReadLine(), out dataDiNascita))
+                {
+                    break;
+                }
+                Console.WriteLine("Data non valida, riprova.");
+            }
+
+            char sesso;
+            while (true)
+            {
+                Console.Write("Inserisci il sesso (M/F): ");
+                if (char.TryParse(Console.ReadLine().ToUpper(), out sesso) && (sesso == 'M' || sesso == 'F'))
+                {
+                    break;
+                }
+                Console.WriteLine("Sesso non valido, inserisci M o F.");
+            }
+
+            Console.Write("Inserisci il codice fiscale: ");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.Write("Inserisci il comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            decimal redditoAnnuale;
+            while (true)
+            {
+                Console.Write("Inserisci il reddito annuale (separare i centesimi con una virgola): ");
+                if (decimal.TryParse(Console.ReadLine(), out redditoAnnuale) && redditoAnnuale > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Reddito non valido, riprova.");
+            }
+
+            Contribuente contribuente = new Contribuente(nome, cognome, dataDiNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+            Console.WriteLine("\n==================================================");
+            Console.WriteLine(" RIEPILOGO DATI CONTRIBUENTE ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(contribuente);
+            Console.WriteLine("\n==================================================");
+
+            Console.Write("\nVuoi calcolare l'imposta per un altro contribuente? (S/N): ");
+            string risposta = Console.ReadLine().ToUpper();
+            if (risposta != "S")
+            {
+                Console.WriteLine("Chiusura del programma...");
+                break;
+            }
+        }
+    }
+}
